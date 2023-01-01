af9e6ac5d5cd7084d8007a39c472d5fb7844f097
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-01 10:56:27 +0530

    Grading System

---
+++ Grading_System.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Grading_System {
+    public static void main(String[] args) {
+        // input - don't change this code
+        Scanner scn = new Scanner(System.in);
+        int marks = scn.nextInt();
+        // input - don't change this code
+        // code here
+        if(marks > 90){
+            System.out.println("excellent");
+        }else if(marks > 80 && marks <= 90){
+            System.out.print("good");
+        }else if(marks > 70 && marks <= 80){
+            System.out.println("fair");
+        }else if(marks > 60 && marks <= 70){
+            System.out.print("meets expectations");
+        }else{
+            System.out.print("below par");
+        }
+    }
+}
