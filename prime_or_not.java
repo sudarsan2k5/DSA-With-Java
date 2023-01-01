fa8b6e4ea31e416b620f88078d92ca3ea2723a18
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-01 11:52:57 +0530

    prime or not

--- prime_or_not.java
+++ prime_or_not.java
@@ -7,7 +7,6 @@ public class prime_or_not {
 
         for(int i = 0; i < t; i++){
             int n = scn.nextInt();
-
             int count = 0;
             for(int div = 2; div * div <= n; div++){
                 if(n % div == 0){
