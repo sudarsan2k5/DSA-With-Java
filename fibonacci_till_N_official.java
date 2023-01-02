8cd37d6469cca5b93511b35e167a18978f497c7f
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-02 22:26:47 +0530

    Fibonacci Till N Official

---
+++ fibonacci_till_N_official.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fibonacci_till_N_official {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int a = 0;
+        int b = 1;
+        for(int i = 0; i < n; i++){
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
