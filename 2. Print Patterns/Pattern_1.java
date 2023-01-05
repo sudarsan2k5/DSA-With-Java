a06c744bf2e90f5252f8f2d7cd550d2880d76a87
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-05 12:56:05 +0530

    Print Pattern

---
+++ 2. Print Patterns/Pattern_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pattern_1 {
+    public static void main(String[] args) {
+        // write ur code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
