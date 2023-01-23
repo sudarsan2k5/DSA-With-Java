01d48fdf432b0c8b14e0face11f4ca412c0fcddf
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-23 19:48:51 +0530

    Bar Chart

---
+++ 3. Function/Bar_Chart.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bar_Chart {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        for(int floor = max; floor >= 1; floor--){
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] >= floor){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
