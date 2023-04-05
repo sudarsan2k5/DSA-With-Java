497cc487cf01c7c329f6563e0bb7bba85c56faa6
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-04-05 12:25:37 +0530

    Print 2D Array

---
+++ 4. Binary Search/print_2d_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class print_2d_array {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int r = scn.nextInt();
+        int c = scn.nextInt();
+
+        int[][] arr = new int[r][c];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
