28c4a48f8659b981c4d70bc1111ee3139be43797
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-05-13 13:14:03 +0530

    Print Decreasing

---
+++ 7. Recursion/Print_Decreasing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Print_Decreasing {
+    public static void print_dic(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        print_dic(n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a Num: ");
+        int n = scn.nextInt();
+        print_dic(n);
+    }
+}
