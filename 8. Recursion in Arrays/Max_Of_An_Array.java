e2028c911c9446570c5680cec31c737272514c7c
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-05-18 12:52:18 +0530

    Max Of An Array

---
+++ 8. Recursion in Arrays/Max_Of_An_Array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Max_Of_An_Array {
+    public static int max_arr(int[] arr, int idx){
+        if(idx == arr.length - 1){
+            return arr[idx];
+        }
+        int misa = max_arr(arr, idx + 1);
+        if(misa > arr[idx]){
+            return misa;
+        }else{
+            return arr[idx];
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max = max_arr(arr, 0);
+        System.out.print(max);
+    }
+}
