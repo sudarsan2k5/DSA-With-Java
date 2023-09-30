d3585e14c33df10fea88c1fcefc2b094c5a12edb
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-09-30 20:01:48 +0530

    Print Increasing

---
+++ 7. Recursion/Revision/Print_Increasing.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Print_Increasing {
+    public static void print_inc(int n){
+        if(n == 0)
+            return;
+        print_inc(n - 1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        print_inc(n);
+    }
+}
