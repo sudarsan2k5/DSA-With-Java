26903a086672d1c46dc8259ba07544041da412d7
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-10-04 01:49:45 +0530

    Last Index

--- 8. Recursion in Arrays/Revision/Last_Index.java
+++ 8. Recursion in Arrays/Revision/Last_Index.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Last_Index {
+import java.util.Scanner;
+
+public class Last_Index {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+    }
 }
