ef1ecebcd0b881f97c72be7c8018fc4f4ce312e9
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2024-08-14 23:01:41 +0530

    Intro LinkList

---
+++ LinkList/linklist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class linklist {
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class linklist{
+        Node head;
+        Node tail;
+        int size;
+    }
+    public static void main(String[] args) {
+    }
+}
